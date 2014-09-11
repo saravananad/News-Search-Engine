0df14caa41756f6706ca46de9d93a8da0e6567a8
nramamur <nramamur@buffalo.edu>
2014-09-10 22:25:20 -0400

    Place, Date and Text content Parsing Modules updated + Minor changes in Title Parsing made.

--- edu/buffalo/cse/irf14/document/Parser.java
+++ edu/buffalo/cse/irf14/document/Parser.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -20,11 +21,11 @@ public class Parser {
 	 * @return The parsed and fully loaded Document object
 	 * @throws ParserException In case any error occurs during parsing
 	 */
-	
 	private static final Pattern authorLinePattern = Pattern.compile("<AUTHOR>(.*)</AUTHOR>");
 	private static final String remove_AUTHOR_Tag_Pattern = "\\s*</*AUTHOR>\\s*";
 	private static final String remove_By_From_Author_Pattern = "\\S*[by|BY|By|bY]\\s+";
-	private static final Pattern titlePattern = Pattern.compile("\\s*[a-zA-Z0-9]?[,-\\.'<>]*");
+	private static final Pattern titlePattern = Pattern.compile("\\s*[a-zA-Z0-9]*;+\\s*");
+	private static final Pattern datePattern = Pattern.compile("\\s*(?:[Jj]an(?:uary)?|[Ff]eb(?:ruary)?|[mM]ar(?:ch)?|[aA]pr(?:il)?|[mM]ay?|[jJ]un(?:e)?|[jJ]ul(?:y)?|[aA]ug(?:ust)?|[sS]ep(?:tember)?|[Oo]ct(?:ober)?|([Nn]ov|[Dd]ec)(?:ember))+\\s*[0-9]{1,2}+\\s*");
 	
 	public static Document parse(String filename) throws ParserException {
 		BufferedReader reader = null;
@@ -33,20 +34,21 @@ public class Parser {
 			Document doc = new Document();
 			if(reader != null) {
 				String line = null;
-				boolean isTitleLine = true, isSecondLine = false;
+				String place = "";
+				String content = "";
+				String publishedDate = "";
+				boolean isTitleLine = true, isSecondLine = true, isPlaceDateLine = true;
+				String[] placeDateString = null;
 				File inputFile = new File(filename);
 				doc.setField(FieldNames.FILEID, inputFile.getParentFile().getName() + "_" + inputFile.getName());
 				doc.setField(FieldNames.CATEGORY, inputFile.getParentFile().getName());
 				while((line = reader.readLine()) != null) {
 					if(!line.isEmpty()) {
 						if(isTitleLine) {
-							if(!titlePattern.matcher(line.trim()).matches()) {
-								//Continue
-								System.out.println(line);
+							if (!(titlePattern.matcher(line).matches())){
+								doc.setField(FieldNames.TITLE, line.trim());
+								isTitleLine = false;	
 							}
-							doc.setField(FieldNames.TITLE, line.trim());
-							isTitleLine = false;
-							isSecondLine = true;
 						} else if(isSecondLine && authorLinePattern.matcher(line).matches()) {
 							line = line.replaceAll(remove_AUTHOR_Tag_Pattern, "").replaceFirst(remove_By_From_Author_Pattern, "");
 							String authorParams[] = line.split(",");
@@ -54,12 +56,43 @@ public class Parser {
 								doc.setField(FieldNames.AUTHORORG, authorParams[1]);
 							}
 							doc.setField(FieldNames.AUTHOR, authorParams[0]);
-						} else {
-							//TODO Parse and get place and date.
 							isSecondLine = false;
+						} else if (isPlaceDateLine){
+							//TODO Parse and get place and date.
+							if (line.contains(" - ")) {
+								String[] contentFirstLine = line.split("-", 2);
+								content += contentFirstLine[contentFirstLine.length - 1].trim();
+								placeDateString = contentFirstLine[0].split(",");
+								int length = placeDateString.length;
+								if (datePattern.matcher(placeDateString[placeDateString.length - 1]).matches()){
+									length = placeDateString.length - 1;
+									publishedDate = placeDateString[placeDateString.length - 1];
+								}
+								for (int i = 0; i < length; i++){
+									place += placeDateString[i];
+								}
+								doc.setField(FieldNames.NEWSDATE, publishedDate);
+							}
+							else {
+								Matcher dateMatch = datePattern.matcher(line);
+								if (dateMatch.find()){
+									doc.setField(FieldNames.NEWSDATE, dateMatch.group().trim());
+									String[] textArray = line.split(dateMatch.group());
+									content += textArray[textArray.length - 1].trim();	
+								}		
+								else {
+									content += line.trim();
+								}
+							}		
+							doc.setField(FieldNames.PLACE, place.trim());
+							isPlaceDateLine = false;
+						}			
+						else {
+							content = content + " " + line.trim();
 						}
 					}
 				}
+				doc.setField(FieldNames.CONTENT, content);
 				return doc;
 			}
 		} catch(IOException ex) {
@@ -70,7 +103,7 @@ public class Parser {
 			} catch (IOException e) {
 				System.err.println(e);
 			}
-		}
+		}	
 		return null;
 	}
 
